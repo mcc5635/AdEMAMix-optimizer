4e80dbd48d2ba373f9356efda8fc85b1d59a31d6
Michael Chase <themikechase@gmail.com>
2024-09-08 13:44:48 -0700

    create initialization of optimizer

--- test.py
+++ test.py
@@ -5,7 +5,16 @@ from torch.optim import Optimizer
 
 class AdEMAMix(Optimizer):
 
-
+    def __init__(self,
+                 params,
+                 lr=le-3,
+                 betas=(0.9,0.999,0.9999),
+                 alpha=5.0,
+                 T_beta3=0,
+                 T_alpha=0,
+                 eps=1e-8,
+                 weight_decay=0.0):
+        # initialize the optimizer
     return loss
 
 
