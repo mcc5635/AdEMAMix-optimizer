cc356d5d2382c1bbce021502da7c4f5e03e8ece7
Michael Chase <themikechase@gmail.com>
2024-09-08 13:43:04 -0700

    skeleton model

---
+++ test.py
@@ -0,0 +1,16 @@
+import math
+import torch
+from torch.optim import Optimizer
+
+
+class AdEMAMix(Optimizer):
+
+
+    return loss
+
+
+
+# AdEMAMix Code Skeleton using Pytorch (Paszke et al., 2019).
+
+
+
