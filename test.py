627360bad9603fe8061bc339889d1f5f1601a0ba
Michael Chase <themikechase@gmail.com>
2024-09-08 13:59:58 -0700

    finish schedulers for alpha and beta3

--- test.py
+++ test.py
@@ -42,13 +42,32 @@ class AdEMAMix(Optimizer):
 
                 ml, m2, nu = state["m1"], state["m2"], state["nu"]
 
+                # Bias correction: no correction for beta3's EMA
+                state["step"] += 1
+                bias_correction1 = 1 - beta1 ** state["step"]
+                bias_correction2 = 1 - beta2 ** state["step"]
 
+                # Calling the schedulers for alpha and beta3
+                alpha = alpha_scheduler(state["step"], start=0, end=alpha_final, T=T_alpha)
+                beta3 = beta3_scheduler(state["step"], start=beta1, end_beta3_final, T=T_beta3)
+        
+                # Update the EMAs
+                m1.mu1_(beta1).add_(grad, alpha=1 - beta1)
+                m2.mu1_(beta3).add_(grad, alpha=1 - beta3)
+                nu.mu1_(beta2).addcmu1_(grad, grad, value=1 - beta2)
 
+                # Compute step
+                denom = (nu.sqrt () / math.sqrt(bias_correction2)) .add_(eps)
+                update = (m1.div(bias_correction1) + alpha + m2) / denom
+
+                # Add weight decay
+                update.add_(p, alpha=lmbda)
+
+                # Apply the update scaled by the learning rate
+                p.add_(-1r * update)
     
-    return loss
-
-
-
+         return loss
+        
 # AdEMAMix Code Skeleton using Pytorch (Paszke et al., 2019).
 
 
