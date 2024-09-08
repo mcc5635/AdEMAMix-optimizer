d472dc85d8eea9b8bbc9e96b7fab5f4c738dfd50
Michael Chase <themikechase@gmail.com>
2024-09-08 13:50:39 -0700

    create state initialization

--- test.py
+++ test.py
@@ -15,6 +15,36 @@ class AdEMAMix(Optimizer):
                  eps=1e-8,
                  weight_decay=0.0):
         # initialize the optimizer
+    @torch.no_grad()
+    def step(self):
+
+        for group in self.param_groups:
+
+            lr = group["lr"]
+            lmbda = group["weight_decay"]
+            eps = group["eps"]
+            beta1, beta2, beta3_final = group["betas"]
+            T_beta3 = group["T_beta3"]
+            T_alpha = group["T_alpha"]
+            alpha_final = group["alpha"]
+
+            for p in group["params"]:
+
+                grad = p.grad
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state["step"] = 0 # step counter used for bias correction
+                    state["m1"] = torch.zeros_like(p) # fast EMA
+                    state["m2"] = torch.zeros_like(p) # slow EMA
+                    state["nu"] = torch.zeros_like(p) # second moment estimate
+
+                ml, m2, nu = state["m1"], state["m2"], state["nu"]
+
+
+
+    
     return loss
 
 
